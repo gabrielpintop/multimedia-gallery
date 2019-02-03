34ecf9d6ab19f7bb60e737065c656a06c4bcf7f0
NATHALIA ALVAREZ <nathalia.alvarez@globant.com>
2019-02-03 01:22:42 -0500

    Clip model creation

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Image, Category, Comment, Multimedia, User
+from .models import Image, Category, Comment, Multimedia, User, Clip
 
 # Register your models here.
 admin.site.register(Image)
@@ -7,3 +7,4 @@ admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(Multimedia)
 admin.site.register(User)
+admin.site.register(Clip)
--- gallery/models.py
+++ gallery/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.forms import ModelForm
 
+
 # Create your models here.
 
-#Tabla categoria
+# User model
 class User(models.Model):
     name = models.CharField(max_length=100)
     lastName = models.CharField(max_length=100)
@@ -13,25 +14,31 @@ class User(models.Model):
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
 
+
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields=['name','lastName','photo','city','country','email','password']
+        fields = ['name', 'lastName', 'photo', 'city', 'country', 'email', 'password']
 
+
+# Category model
 class Category(models.Model):
     name = models.CharField(max_length=100, null=False)
+
 
 class CategoryForm(ModelForm):
     class Meta:
         model = Category
-        fields=['name']
+        fields = ['name']
 
+
+# Multimedia model
 class Multimedia(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=50)
-    userId = models.ForeignKey(User,on_delete=models.CASCADE)
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
     creationDate = models.CharField(max_length=20, null=True)
-    idCategory = models.ForeignKey(Category,on_delete=models.CASCADE)
+    idCategory = models.ForeignKey(Category, on_delete=models.CASCADE)
     typeID = models.CharField(max_length=1000)
     city = models.CharField(max_length=100, null=True)
     country = models.CharField(max_length=100, null=True)
@@ -40,10 +47,12 @@ class Multimedia(models.Model):
     def __str__(self):
         return Multimedia
 
+
 class MultimediaForm(ModelForm):
     class Meta:
         model = Multimedia
-        fields = ['title', 'author', 'userId','creationDate', 'idCategory', 'typeID', 'city', 'country', 'url']
+        fields = ['title', 'author', 'userId', 'creationDate', 'idCategory', 'typeID', 'city', 'country', 'url']
+
 
 class Image(models.Model):
     name = models.CharField(max_length=200)
@@ -61,14 +70,27 @@ class ImageForm(ModelForm):
         fields = ['name', 'url', 'description', 'type']
 
 
-
-#Tabla comentarios
+# Comment model
 class Comment(models.Model):
     comment = models.CharField(max_length=300, null=False)
     userId = models.CharField(max_length=100, null=False)
 
-class  CommentForm(ModelForm):
+
+class CommentForm(ModelForm):
     class Meta:
         model = Comment
-        fields =['comment','userId']
+        fields = ['comment', 'userId']
 
+
+# Clip model
+class Clip(models.Model):
+    name = models.CharField(max_length=200)
+    initialSec = models.SmallIntegerField(default=0)
+    finalSec = models.SmallIntegerField(default=0)
+    userId = models.CharField(max_length=100, null=False)
+
+
+class ClipForm(ModelForm):
+    class Meta:
+        model = Clip
+        fields = ['name', 'initialSec', 'finalSec', 'userId']
