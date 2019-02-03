b52d60a12aacf9b778f817e0473d30394680bda3
jprietouniandes <47231655+jprietouniandes@users.noreply.github.com>
2019-02-02 23:17:52 -0500

    Creacion de clase multimedia
    Se hace migrate de los campos y se crear clase usuario ficticia para que compile el proyecto. Adicionalmente ignoro cambios automaticos que obtuve en archivos xml.

--- gallery/models.py
+++ gallery/models.py
@@ -3,6 +3,47 @@ from django.forms import ModelForm
 
 # Create your models here.
 
+#Tabla categoria
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    photo = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields=['name','lastName','photo','city','country','email','password']
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False)
+
+class CategoryForm(ModelForm):
+    class Meta:
+        model = Category
+        fields=['name']
+
+class Multimedia(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=50)
+    userId = models.ForeignKey(User,on_delete=models.CASCADE)
+    CreationDate = models.CharField(max_length=20, null=True)
+    IdCategory = models.ForeignKey(Category,on_delete=models.CASCADE)
+    typeID = models.CharField(max_length=1000)
+    City = models.CharField(max_length=100, null=True)
+    Country = models.CharField(max_length=100, null=True)
+    url = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return Multimedia
+
+class MultimediaForm(ModelForm):
+    class Meta:
+        model = Multimedia
+        fields = ['title', 'author', 'userId','CreationDate', 'IdCategory', 'typeID', 'City', 'Country', 'url']
 
 class Image(models.Model):
     name = models.CharField(max_length=200)
@@ -19,14 +60,7 @@ class ImageForm(ModelForm):
         model = Image
         fields = ['name', 'url', 'description', 'type']
 
-#Tabla categoria
-class Category(models.Model):
-    name = models.CharField(max_length=100, null=False)
 
-class CategoryForm(ModelForm):
-    class Meta:
-        model = Category
-        fields=['name']
 
 #Tabla comentarios
 class Comment(models.Model):
@@ -37,3 +71,4 @@ class  CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields =['comment','userId']
+
