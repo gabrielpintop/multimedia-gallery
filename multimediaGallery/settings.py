2c5e2a7b5683f6b4404cf84e8b81cc850db512d5
Tatiana Macias Cardenas <tcardenas@localhost.localdomain>
2019-02-11 22:02:55 -0500

    Prueba host

--- multimediaGallery/settings.py
+++ multimediaGallery/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
