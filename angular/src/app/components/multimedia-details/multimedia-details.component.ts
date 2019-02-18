d7d2d452fb4e6e8e5571524831946843c0c28776
NATHALIA ALVAREZ <nathalia.alvarez@globant.com>
2019-02-17 21:51:48 -0500

    implementation of set clips service

--- angular/src/app/components/multimedia-details/multimedia-details.component.ts
+++ angular/src/app/components/multimedia-details/multimedia-details.component.ts
@@ -1,3 +1,4 @@
+import { ClipsService } from 'src/app/services/clips/clips.service';
 import {
   Component,
   OnInit,
@@ -20,12 +21,14 @@ export class MultimediaDetailsComponent implements OnInit, OnChanges {
 
   @Output() private closeModal = new EventEmitter<boolean>();
 
-  constructor() {}
+  constructor(
+    private clipsService: ClipsService
+  ) {}
 
   public clip = {
     name: '',
-    startSecond: 0,
-    endSecond: 0
+    initialSec: 0,
+    finalSec: 0
   };
 
   public selectedClip = null;
@@ -49,23 +52,7 @@ export class MultimediaDetailsComponent implements OnInit, OnChanges {
   public startTime = 0;
   public endTime = Number.MAX_VALUE;
 
-  private clipsOptions = [
-    {
-      name: 'First clip',
-      startSecond: 0,
-      endSecond: 12
-    },
-    {
-      name: 'Second clip',
-      startSecond: 4,
-      endSecond: 12
-    },
-    {
-      name: 'Third clip',
-      startSecond: 8,
-      endSecond: 10
-    }
-  ];
+  private clipsOptions = [];
 
   public urlToPlay = '';
 
@@ -77,29 +64,56 @@ export class MultimediaDetailsComponent implements OnInit, OnChanges {
     if (changes.multimedia.currentValue) {
       this.originalUrl = changes.multimedia.currentValue.url;
       this.urlToPlay = this.originalUrl;
+      this.loadClips();
     }
+  }
+
+  loadClips() {
+    console.log(this.multimedia);
+    this.clipsService
+      .getClips(this.multimedia)
+      .then((data: any) => {
+        this.clipsOptions = data.fields;
+        console.log(this.clipsOptions)
+        this.clipsOptions.sort(this.compare);
+        this.clips = this.clipsOptions;
+      })
+      .catch(err => {
+        alert(err);
+      });
+  }
+
+  compare(a, b) {
+    if (a.name < b.name) {
+      return 1;
+    }
+    if (a.name > b.name) {
+      return -1;
+    }
+    return 0;
   }
 
   setDuration(media) {
     this.duration = Math.floor(media.duration);
-    this.clip.endSecond = this.duration;
+    this.clip.finalSec = this.duration;
     this.loadedMetadata = true;
     this.clips = this.clipsOptions;
   }
 
   onSubmit() {
+
     if (this.clip.name === '') {
       this.clipFormErrorMessage = 'The name of the clip is required';
       this.clipFormError = true;
-    } else if (this.clip.startSecond >= this.duration) {
+    } else if (this.clip.initialSec >= this.duration) {
       this.clipFormErrorMessage =
         'The start second can´t be equal or greater to the video duration';
       this.clipFormError = true;
-    } else if (this.clip.endSecond <= 0) {
+    } else if (this.clip.finalSec <= 0) {
       this.clipFormErrorMessage =
         'The end second can´t be equal or less than 0';
       this.clipFormError = true;
-    } else if (this.clip.endSecond <= this.clip.startSecond) {
+    } else if (this.clip.finalSec <= this.clip.initialSec) {
       this.clipFormErrorMessage =
         'The end second can´t be equal or less than the start second';
       this.clipFormError = true;
@@ -111,8 +125,8 @@ export class MultimediaDetailsComponent implements OnInit, OnChanges {
 
       setTimeout(() => {
         this.clip.name = '';
-        this.clip.startSecond = 0;
-        this.clip.endSecond = this.duration;
+        this.clip.initialSec = 0;
+        this.clip.finalSec = this.duration;
         this.clipLoadingForm = false;
         this.clipSubmitSuccess = true;
       }, 1000);
@@ -129,7 +143,7 @@ export class MultimediaDetailsComponent implements OnInit, OnChanges {
     console.log(clip);
     this.selectedClip = clip;
     this.urlToPlay =
-      this.originalUrl + '#t=' + clip.startSecond + ',' + clip.endSecond;
+      this.originalUrl + '#t=' + clip.initialSec + ',' + clip.finalSec;
     document.getElementById('mediaBody').scrollIntoView();
   }
 
---
+++ angular/src/app/services/clips/clips.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClipsService {
+  constructor(private http: HttpClient) {}
+
+  getClips(multimedia) {
+    console.log(multimedia.id);
+    return new Promise((resolve, reject) => {
+      const options = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      };
+
+      this.http
+        .get(environment.requestBase + '/api/clips/' + multimedia.id, options)
+        .subscribe(
+          (data: any) => {
+            console.log(data);
+
+            resolve(data);
+          },
+          err => {
+            reject('There was an error getting the clips');
+          }
+        );
+    });
+  }
+}
