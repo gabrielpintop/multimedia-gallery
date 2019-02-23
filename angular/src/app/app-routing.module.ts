d045d12db4705d9e37ce656dc22386f00874d94d
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-02-23 15:01:19 -0500

    Authentication guard is implemented

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MultimediaListComponent } from './components/multimedia-list/multimedia-list.component';
+import { MultimediaAddComponent } from './components/multimedia-add/multimedia-add.component';
+import { AuthenticationGuard } from './guards/authentication.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: MultimediaListComponent
+  },
+  {
+    path: 'addMultimedia',
+    canActivate: [AuthenticationGuard],
+    component: MultimediaAddComponent
   },
   { path: '**', redirectTo: '' }
 ];
---
+++ angular/src/app/components/multimedia-add/multimedia-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multimedia-add',
+  templateUrl: './multimedia-add.component.html',
+  styleUrls: ['./multimedia-add.component.css']
+})
+export class MultimediaAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
